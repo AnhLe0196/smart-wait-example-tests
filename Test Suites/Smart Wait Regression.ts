b1fbc7fd14a209bcae3bf0d7abee3d674fc8932d
sangbui <sangbui@sgh232q0xb.kms.com.vn>
2019-11-18 15:38:52 +0700

    Smart wait test cases

---
+++ Test Suites/Smart Wait Regression.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smart Wait Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>16df2f63-fd87-49a8-bbf6-a116c7961c75</testSuiteGuid>
+   <testCaseLink>
+      <guid>833e03d6-261b-4c04-9c92-66ccbae2ea5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Apply All Elements - Verify AJAX Text Present</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6d9bc66b-45c3-4d8b-a8f5-d051ac574f21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Apply Specific Elementsts - Verify AJAX Text Present</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
